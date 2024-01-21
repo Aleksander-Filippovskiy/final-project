7b1599b485c9a72e6575f82f50312aacc493d698
Aleksander-Filippovskiy <153448124+Aleksander-Filippovskiy@users.noreply.github.com>
2024-01-21 23:25:30 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# configuration.py
+
+# Изменяющийся URL
+URL_SERVICE = "https://ee678383-7d75-4504-9277-ad0054b4f19a.serverhub.praktikum-services.ru"
+
+# Ручки
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track?t="
